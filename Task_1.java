4f596b8f45fc7033b06ed52125e7ee5fe01c4cd0
Tanvi bansal <tanvibansal099@gmail.com>
2025-07-05 20:46:59 +0530

    Add files via upload

---
+++ Task_1.java
@@ -0,0 +1,11 @@
+package CodSoft_Project;
+//import java.util.Scanner;
+public class Task_1 {
+    public static void main(String[] args) {
+        int random_Num = (int) Math.random();
+        System.out.println(random_Num);
+        System.out.println(1);
+    }
+}
+
+
